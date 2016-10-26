efc2b2a9d23acbc59909dd3aa65d8e35ec71742e
ecmcbrid <kaliserna@Samuels-MacBook-Air.local>
2016-10-26 12:27:56 -0500

    project 2
    Internet tech project 2 progress

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+'use strict';
+
+
+$(function() {
+
+
+    //settings for slider
+    var width = 1000;
+    var animationSpeed = 300;
+    var pause = 2000;
+    var currentSlide = 1;
+
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+
+    var interval;
+
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-=' + width},animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-center', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+
+    startSlider();
+
+
+
+
+});
+
+
+
+
